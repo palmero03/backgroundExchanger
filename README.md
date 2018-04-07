e0f8ebd15dfbf33a0acd7c6fee30ce5205506889
Darwin Palmero <darwinpalmero3@gmail.com>
2018-04-07 16:19:47 -0400

    Added electron-packager

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
 
+##pre requisites for Windows
+
+```bash
+You need to install windows build tools, execute this command on a Powershell with administrator rights:
+npm install --global --production windows-build-tools
+```
+
 ```bash
 # Clone this repository
 git clone https://github.com/palmero03/backgroundExchanger.git
@@ -13,3 +20,20 @@ npm install
 npm start
 ```
 
+## To Generate installers
+
+
+```bash
+# Install packager
+npm install electron-packager -g
+# Go into the app
+cd backgroundExchanger
+# For Mac
+npm run package-mac
+# For Linux
+npm run package-linux
+# For Windows 32bits
+npm run package-win-32
+# For Windows 64bits
+npm run package-win-64
+```
--- renderer.js
+++ renderer.js
@@ -5,40 +5,42 @@
 const serialport = require('serialport')
 const createTable = require('data-table')
 
+var portsStr = '';
 
 serialport.list((err, ports) => {
-  console.log('ports', ports);
-  if (err) {
-    document.getElementById('error').textContent = err.message
-    return
-  } else {
-    document.getElementById('error').textContent = ''
-  }
+	console.log('ports', ports);
+	if (err) {
+		document.getElementById('error').textContent = err.message;
+		return;
+	} else {
+		document.getElementById('error').textContent = ''
+	}
 
-  if (ports.length === 0) {
-    document.getElementById('error').textContent = 'No ports discovered'
-  }
-   
-  ports.forEach(function(port) {
-	var sp = new serialport(port.comName,{baudrate: 9600, autoOpen: false, 
-						parser: serialport.parsers.readline("\r")}); 	
-	sp.open(function (error) { 
-		if ( !error ) { 
-			console.log('open'); 
-			sp.on('data', function(data) { 
-				console.log('data received: ' + 						port.comName+ ' ' + data);
-				document.getElementById('ports')
-					.innerHTML+='Received: '+data+'<br/>';
-				sp.write('ok', function(err) {
-					if(err) {
-						console.log('error port: ' + port.comName);
-					} else {
-						console.log('sended ok to ' + port.comName);
-					}			
-				}); 
-			});
-			 
-		} 
-	}); 
-  })  
-})
+	if (ports.length === 0) {
+		document.getElementById('error').textContent = 'No ports discovered'
+	}
+
+	ports.forEach(function(port) {
+		var sp = new serialport(port.comName,{baudrate: 9600, autoOpen: false, parser: serialport.parsers.readline("\n")}); 	
+		sp.open(function (error) { 
+			if ( !error ) { 
+				console.log('open port: ' + port.comName);
+				portsStr += 'Port Opened: ' + port.comName + '<br/>';	
+				document.getElementById('ports').innerHTML = 'COM Port connection Test:<br/>' + portsStr + 'Waiting for Messages:<br/>'				
+				sp.on('data', function(data) {
+					console.log('data received: ' + port.comName + ' ' + data);
+					document.getElementById('ports').innerHTML+='Received: ' + data + '<br/>';
+					sp.write('ok', function(err) {
+						if(err) {
+							console.log('error port: ' + port.comName);
+						} else {
+							console.log('sended ok to ' + port.comName);
+						}			
+					}); 
+				});
+				 
+			} 
+		}); 
+	});
+	//document.getElementById('ports').innerHTML += portsStr + 'Waiting for Messages:<br/>';  
+});
