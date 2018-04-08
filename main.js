7666e6c476512075a7d00fadf3271b13eed6aa62
Darwin Palmero <darwinpalmero3@gmail.com>
2018-04-08 15:38:43 -0400

    added connections to test websocket

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const electron = require('electron');
 const ipcMain  = require('electron').ipcMain;
+const serialport = require('serialport');
+const WebSocket = require('ws');
+
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -10,6 +13,12 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+var currentComPort = null;
+var user = '';
+var pwd = '';
+var xpub = '';
+var wid = '';
+var devid = '';
 
 function createWindow () {
   // Create the browser window.
@@ -54,9 +63,101 @@ app.on('activate', function () {
   }
 })
 
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
  ipcMain.on('form-submission', function (event, data) {
     console.log("user ->", data.user);
-	mainWindow.webContents.send('searchPorts', data);
+	user = data.user;
+	//Call API signin
+	searchPorts();
+	sendWalletConnected();
 });
+
+function searchPorts() { 
+	serialport.list((err, ports) => {
+		var portsStr = '';
+		console.log('ports', ports);
+		if (err) {
+			mainWindow.webContents.send('errorMsg', err.message);
+			return;
+		}
+		if (ports.length === 0) {
+			mainWindow.webContents.send('errorMsg', 'No ports discovered');
+		}
+		ports.forEach(function(port) {
+			var sp = new serialport(port.comName,{baudrate: 9600, autoOpen: false, parser: serialport.parsers.readline("\n")}); 	
+			sp.open(function (error) { 
+				if ( !error ) { 
+					console.log('open port: ' + port.comName);
+					portsStr += 'Port Opened: ' + port.comName + '<br/>';
+					let infoData = {
+						usr: user,
+						info: '<br/>' + portsStr + 'Waiting for Messages:<br/>'
+					};
+					mainWindow.webContents.send('renderPorts', infoData);
+					sp.on('data', function(data) {
+						console.log('data received: ' + port.comName + ' ' + data);
+						if (currentComPort == null) {
+							currentComPort = sp;
+						} else if (currentComPort != sp) {
+							console.log('data already receiving from: ' + currentComPort);
+							return;
+						}
+						if(data.startsWith('xpub')) {
+							xpub = data.split(':')[1];
+							sendOK(sp);	
+						} else if (data.startsWith('wid')) {
+							wid = data.split(':')[1];
+							sendOK(sp);
+						} else if (data.startsWith('devid')) {
+							devid = data.split(':')[1];
+							sendOK(sp);
+						} else if (data == 'get:unsigned' && xpub != '' && wid != '' && devid != '') {
+							//Call websocket wallet_connected
+							sendWalletConnected();
+						}
+					});
+					 
+				} 
+			}); 
+		});
+	});	
+}
+
+function sendOK(sp) {
+	sp.write('ok', function(err) {
+		if(err) {
+			console.log('error port: ' + port.comName);
+		} else {
+			console.log('sended ok to ' + port.comName);
+		}			
+	});
+}
+
+
+function sendWalletConnected() {
+	let ws = new WebSocket('ws://echo.websocket.org/');
+	console.log('started connection');
+
+	ws.on('open', function open() {
+	  console.log('connected to ws://echo.websocket.org/');
+	  ws.emit('wallet_connected',{ message:{"xpub_payer": xpub, "wallet_id": wid,"device_id": devid} });
+	  ws.send('testing 1');
+	});
+
+	ws.on('send_unsigned', function (data) {
+	  console.log(data);
+	  sendUnsignedTxn(data.message);
+	}); 
+}
+
+function sendUnsignedTxn(data) {
+	currentComPort.write('unsigned:' + data.unsigned_txn, function(err) {
+		if(err) {
+			console.log('error port: ' + currentComPort);
+		} else {
+			console.log('sended '+data.unsigned_txn + ' to ' + currentComPort);
+		}			
+	});
+}
--- renderer.js
+++ renderer.js
@@ -6,59 +6,20 @@ const serialport = require('serialport')
 const createTable = require('data-table')
 const ipcRenderer = require('electron').ipcRenderer;
 
-var user = '';
-var pwd = '';
-
-ipcRenderer.on('searchPorts', (event, data) => {
-	user = data.user;
-	pwd = data.password;
-	document.getElementById('login').textContent = '';
-	
-	serialport.list((err, ports) => {
-		var portsStr = '';
-		console.log('ports', ports);
-		if (err) {
-			document.getElementById('error').textContent = err.message;
-			return;
-		} else {
-			document.getElementById('error').textContent = ''
-		}
-
-		if (ports.length === 0) {
-			document.getElementById('error').textContent = 'No ports discovered'
-		}
-
-		ports.forEach(function(port) {
-			var sp = new serialport(port.comName,{baudrate: 9600, autoOpen: false, parser: serialport.parsers.readline("\n")}); 	
-			sp.open(function (error) { 
-				if ( !error ) { 
-					console.log('open port: ' + port.comName);
-					portsStr += 'Port Opened: ' + port.comName + '<br/>';	
-					renderPorts('COM Port connection Test:<br/>' + portsStr + 'Waiting for Messages:<br/>');
-					sp.on('data', function(data) {
-						console.log('data received: ' + port.comName + ' ' + data);
-						document.getElementById('ports').innerHTML+='Received: ' + data + '<br/>';
-						sp.write('ok', function(err) {
-							if(err) {
-								console.log('error port: ' + port.comName);
-							} else {
-								console.log('sended ok to ' + port.comName);
-							}			
-						}); 
-					});
-					 
-				} 
-			}); 
-		});
-		//document.getElementById('ports').innerHTML += portsStr + 'Waiting for Messages:<br/>';  
-	});	
+ipcRenderer.on('error', (event, data) => {
+	document.getElementById('error').textContent = data;
 });
- 
-function renderPorts(portsStr) {
+
+ipcRenderer.on('renderPorts', (event, data) => {
+	document.getElementById('login').textContent = '';
+	renderPorts(data);
+});
+
+function renderPorts(data) {
 	str = '<div class="card">'+
-					'<div class="card-header">Logged as '+user+'</div>'+
+					'<div class="card-header">Logged as '+data.usr+'</div>'+
 					'<div class="card-body">'+
-					portsStr +
+					data.info +
 					'</div>'+
 				'</div>';
 	document.getElementById('ports').innerHTML = str;
